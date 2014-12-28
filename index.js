7d13da1f3de3c676e97aaae1c2c0625880a49143
yuanqing <hello@yuanqing.sg>
2014-12-28 23:25:53 +0800

    Refactor; allow `opts` to be the second argument when grouping by object value

--- index.js
+++ index.js
@@ -3,61 +3,66 @@
 var deepEqual = require('deep-equal');
 var get = require('jaunt').get;
 
+var isObject = function(obj) {
+  return obj !== null && typeof obj === 'object' && !Array.isArray(obj);
+};
+
 var grouper = function(arr, props, opts) {
 
-  opts = opts || {};
-  if (typeof opts.strict === 'undefined') {
-    opts.strict = true; // default to strict `===`
-  }
-
-  props = [].concat(props).filter(Boolean);
-  var propsLen = props.length;
   var comparator;
-  if (propsLen === 0) {
-    comparator = function(a, b) {
-      return deepEqual(a, b, opts);
-    };
+  if (typeof props === 'function') {
+    comparator = props;
   } else {
-    comparator = function(a, b, props) {
-      var k = -1;
-      while (++k < propsLen) {
-        // check if `a` has the same values as `b` for every property in `props`
-        var prop = props[k];
-        if (!deepEqual(get(a, prop), get(b, prop), opts)) {
-          return false;
+    if (isObject(props)) { // argument shuffling for grouping by object value
+      opts = props;
+      props = [];
+    }
+    opts = opts || {};
+    if (typeof opts.strict === 'undefined') {
+      opts.strict = true; // default to strict `===`
+    }
+    props = [].concat(props).filter(Boolean);
+    var propsLen = props.length;
+    if (propsLen === 0) {
+      comparator = function(a, b) {
+        return deepEqual(a, b, opts);
+      };
+    } else {
+      comparator = function(a, b) {
+        var k = -1;
+        while (++k < propsLen) {
+          // check if `a` has the same values as `b` for every property in `props`
+          var prop = props[k];
+          if (!deepEqual(get(a, prop), get(b, prop), opts)) {
+            return false;
+          }
         }
-      }
-      return true;
-    };
+        return true;
+      };
+    }
   }
 
   var result = [];
-
-  // for every `arr[i]` in `arr`...
   var arrLen = arr.length;
   var i = -1;
+  // for every `arr[i]` in `arr`...
   while (++i < arrLen) {
-
-    // for every `result[j]` group in `result`...
     var wasAdded = false;
     var resultLen = result.length;
     var j = -1;
+    // for every `result[j]` group in `result`...
     while (++j < resultLen) {
-
       // check if `arr[i]` belongs to the `result[j]` group
-      if (comparator(arr[i], result[j][0], props)) {
+      if (comparator(arr[i], result[j][0])) {
         result[j].push(arr[i]);
         wasAdded = true;
       }
-
     }
-
-    // if `arr[i]` was not added to any group, create a new group containing only `arr[i]`
-    // and add it to `result`
+    // if `arr[i]` was not added to any group, create a new group containing only
+    // `arr[i]` and add it to `result`
     if (!wasAdded) {
       result.push([arr[i]]);
     }
-
   }
 
   return result;
--- test/grouper.spec.js
+++ test/grouper.spec.js
@@ -3,45 +3,9 @@
 
 var grouper = require('..');
 
-describe('grouper(arr [, props, opts])', function() {
+describe('grouper', function() {
 
-  describe('grouping on object value', function() {
-
-    var arr = [
-      { foo: true },
-      { foo: 1 },
-      { foo: false },
-      { foo: 1 }
-    ];
-
-    it('defaults to strict `===`', function() {
-      var result = grouper(arr);
-      expect(result).toEqual([
-        [ arr[0] ],
-        [ arr[1], arr[3] ],
-        [ arr[2] ]
-      ]);
-      expect(result[0][0]).toBe(arr[0]);
-      expect(result[1][0]).toBe(arr[1]);
-      expect(result[1][1]).toBe(arr[3]);
-      expect(result[2][0]).toBe(arr[2]);
-    });
-
-    it('can also use coercive `==`', function() {
-      var result = grouper(arr, [], { strict: false });
-      expect(result).toEqual([
-        [ arr[0], arr[1], arr[3] ],
-        [ arr[2] ]
-      ]);
-      expect(result[0][0]).toBe(arr[0]);
-      expect(result[0][1]).toBe(arr[1]);
-      expect(result[0][2]).toBe(arr[3]);
-      expect(result[1][0]).toBe(arr[2]);
-    });
-
-  });
-
-  describe('grouping on object `props`', function() {
+  describe('group by one or more properties', function() {
 
     var arr = [
       { foo: true },
@@ -63,7 +27,7 @@ describe('grouper(arr [, props, opts])', function() {
       expect(result[2][0]).toBe(arr[2]);
     });
 
-    it('can also use coercive `==`', function() {
+    it('with coercive `==`', function() {
       var result = grouper(arr, 'foo', { strict: false });
       expect(result).toEqual([
         [ arr[0], arr[1], arr[3] ],
@@ -75,7 +39,7 @@ describe('grouper(arr [, props, opts])', function() {
       expect(result[1][0]).toBe(arr[2]);
     });
 
-    it('can group on multiple properties', function() {
+    it('group by multiple properties', function() {
       var arr = [
         { title: 'foo', date: { year: 1999, month: 1 } },
         { title: 'foo', date: { year: 2000, month: 1 } },
@@ -88,6 +52,68 @@ describe('grouper(arr [, props, opts])', function() {
         [ arr[1] ],
         [ arr[2], arr[3] ]
       ]);
+    });
+
+  });
+
+  describe('group by object value', function() {
+
+    var arr = [
+      { foo: true },
+      { foo: 1 },
+      { foo: false },
+      { foo: 1 }
+    ];
+
+    it('defaults to strict `===`', function() {
+      var result = grouper(arr);
+      expect(result).toEqual([
+        [ arr[0] ],
+        [ arr[1], arr[3] ],
+        [ arr[2] ]
+      ]);
+      expect(result[0][0]).toBe(arr[0]);
+      expect(result[1][0]).toBe(arr[1]);
+      expect(result[1][1]).toBe(arr[3]);
+      expect(result[2][0]).toBe(arr[2]);
+    });
+
+    it('with coercive `==`', function() {
+      var result = grouper(arr, { strict: false });
+      expect(result).toEqual([
+        [ arr[0], arr[1], arr[3] ],
+        [ arr[2] ]
+      ]);
+      expect(result[0][0]).toBe(arr[0]);
+      expect(result[0][1]).toBe(arr[1]);
+      expect(result[0][2]).toBe(arr[3]);
+      expect(result[1][0]).toBe(arr[2]);
+    });
+
+  });
+
+  describe('group using a comparator function', function() {
+
+    var arr = [
+      { foo: true },
+      { foo: 1 },
+      { foo: false },
+      { foo: 1 }
+    ];
+
+    it('works', function() {
+      var result = grouper(arr, function(a, b) {
+        return a.foo === b.foo;
+      });
+      expect(result).toEqual([
+        [ arr[0] ],
+        [ arr[1], arr[3] ],
+        [ arr[2] ]
+      ]);
+      expect(result[0][0]).toBe(arr[0]);
+      expect(result[1][0]).toBe(arr[1]);
+      expect(result[1][1]).toBe(arr[3]);
+      expect(result[2][0]).toBe(arr[2]);
     });
 
   });
